5e28893108698a37c6121ef156e3f733eeff188e
Luka Stolyarov <luka@topspinmedia.com>
2011-05-07 16:53:10 -0700

    moved results to a new action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
-    return unless request.post? 
-    render :partial => 'search_results', :locals => {:response => current_search.resolve(params)}
+  end
+
+  def search_results
+    render :partial => :search_results, :locals => {:response => current_search.resolve(params)} 
   end
 end
