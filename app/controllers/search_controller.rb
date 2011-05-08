de162bb9f5c4f7b5469ff33dfdbc1378e674e643
Luka Stolyarov <luka@topspinmedia.com>
2011-05-07 19:32:00 -0700

    removed debugs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,6 @@ class SearchController < ApplicationController
   end
 
   def search_results
-    logger.tempdebug "current_search is #{current_search.inspect}"
-    logger.tempdebug "current_search resolve is is #{current_search.resolve(params).inspect}"
     render :partial => 'search_results', :locals => { :response_data => current_search.resolve(params) }
   end
 end
